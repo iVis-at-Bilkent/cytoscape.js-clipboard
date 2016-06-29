425f371802bb1be3dcbc633654a8336205553560
mrsfy <mrsfy@outlook.com>
2016-06-29 13:48:45 +0300

    Check for item in clipboard

--- cytoscape-clipboard.js
+++ cytoscape-clipboard.js
@@ -32,21 +32,26 @@
 
 
             var counter = 0;
+
             function guid() {
                 function s4() {
                     return Math.floor((1 + Math.random()) * 0x10000)
                         .toString(16)
                         .substring(1);
                 }
+
                 return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
                     s4() + '-' + s4() + s4() + s4();
             }
+
             var lastGuid;
+
             function getItemId(last) {
                 return last ? "item_" + counter : "item_" + ++counter;
             }
 
             var cloneCounter = 0;
+
             function getCloneId(last) {
                 if (!last)
                     lastGuid = guid();
@@ -58,24 +63,24 @@
                 if (jsons.length == 0)
                     return [];
                 var oldIdToNewId = {};
-                for (var i = 0; i < jsons.length; i++){
+                for (var i = 0; i < jsons.length; i++) {
                     var json = jsons[i];
                     var id = getCloneId();
                     oldIdToNewId[json.data.id] = id;
                     json.data.id = id;
                 }
 
-                for (var i = 0; i < jsons.length; i++){
+                for (var i = 0; i < jsons.length; i++) {
                     var json = jsons[i];
                     console.log(json);
                     var fields = ["source", "target", "parent"];
-                    for (var k = 0 ; k < fields.length; k++){
+                    for (var k = 0; k < fields.length; k++) {
                         var field = fields[k];
                         if (json.data[field])
                             json.data[field] = oldIdToNewId[json.data[field]];
 
                     }
-                    if(json.position.x) {
+                    if (json.position.x) {
                         json.position.x += 50;
                         json.position.y += 50;
                     }
@@ -94,23 +99,26 @@
                     clipboard[id] = eles.jsons();
                     return _instance;
                 },
-                    paste: function (_id) {
-                        var id = _id ? _id : getItemId(true);
+                paste: function (_id) {
+                    var id = _id ? _id : getItemId(true);
+                    var res = null;
+                    if (clipboard[id]) {
                         var newElesJsons = changeIds(clipboard[id]);
                         var nodes = $.grep(newElesJsons, function (ele) {
-                           return ele.group == "nodes";
+                            return ele.group == "nodes";
                         });
                         var edges = $.grep(newElesJsons, function (ele) {
                             return ele.group == "edges";
                         });
-                        var res;
                         cy.batch(function () {
                             res = cy.add(nodes).union(cy.add(edges));
                             res.select();
                         });
-                        return res;
+
                     }
-                };
+                    return res;
+                }
+            };
 
 
             return _instance; // chainability
