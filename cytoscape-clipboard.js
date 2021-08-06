d04c50655a89c245e585d89c23a14f61fb32d1ff
AsmaJodeiri <86617070+AsmaJodeiri@users.noreply.github.com>
2021-08-06 15:07:02 +0300

    fixed #8

--- cytoscape-clipboard.js
+++ cytoscape-clipboard.js
@@ -13,9 +13,10 @@
 
             var cy = this;
 
-            //Global variables to hold x and y coordinates in case of pasting
+
+            //Global variables to hold x and y coordinates in case of pasting//
             var mouseX, mouseY;
-            cy.on('mousemove', function onmousemove (e) {
+            cy.on('mousemove', function onmousemove(e) {
                 var pos = e.position || e.cyPosition;
                 mouseX = pos.x;
                 mouseY = pos.y;
@@ -26,7 +27,9 @@
                 beforeCopy: null,
                 afterCopy: null,
                 beforePaste: null,
-                afterPaste: null
+                afterPaste: null,
+                afterCut: null,
+                beforeCut: null
             };
 
             $.extend(true, options, opts);
@@ -34,7 +37,7 @@
 
             function getScratch() {
                 if (!cy.scratch("_clipboard")) {
-                    cy.scratch("_clipboard", { });
+                    cy.scratch("_clipboard", {});
 
                 }
                 return cy.scratch("_clipboard");
@@ -56,7 +59,6 @@
                     s4() + '-' + s4() + s4() + s4();
             }
 
-
             function getItemId(last) {
                 return last ? "item_" + counter : "item_" + (++counter);
             }
@@ -68,13 +70,21 @@
 
             var oldIdToNewId = {};
 
-            function changeIds(jsons, pasteAtMouseLoc) {
+            function changeIds(jsons, pasteAtMouseLoc, cuted) {
                 jsons = $.extend(true, [], jsons);
+
                 for (var i = 0; i < jsons.length; i++) {
                     var jsonFirst = jsons[i];
-                    var id = getCloneId();
-                    oldIdToNewId[jsonFirst.data.id] = id;
-                    jsonFirst.data.id = id;
+
+                    if (!cuted) {
+                        var id = getCloneId();
+                        oldIdToNewId[jsonFirst.data.id] = id;
+                        jsonFirst.data.id = id;
+                    } else {
+                        var id = jsonFirst.data.id
+
+                    }
+                   
                 }
 
                 //Paste the elements centered at the mouse location
@@ -83,8 +93,7 @@
                 var centerX, centerY;
                 var diffX, diffY;
                 //Checks only for nodes
-                if (jsons[0] !== undefined && jsons[0].position.x)
-                {
+                if (jsons[0] !== undefined && jsons[0].position.x) {
                     topLeftX = jsons[0].position.x;
                     topLeftY = jsons[0].position.y;
                     bottomRightX = jsons[0].position.x;
@@ -106,12 +115,36 @@
                             bottomRightY = ele.position.y;
                         }
                     }
-                    centerX = (topLeftX+bottomRightX)/2;
-                    centerY = (topLeftY+bottomRightY)/2;
+                    centerX = (topLeftX + bottomRightX) / 2;
+                    centerY = (topLeftY + bottomRightY) / 2;
 
-                    diffX = mouseX -centerX;
+                    diffX = mouseX - centerX;
                     diffY = mouseY - centerY;
                 }
+
+
+               if(cuted) {
+                   var visibleNodes = cy.$(":visible");
+
+                   
+
+                   var inside = false;
+                   var hoveringNode = null;
+
+                   visibleNodes.map((node) => {
+                       var posX = node.position().x;
+                       var posY = node.position().y;
+
+                       if (mouseX < posX+ 50 && mouseX > posX - 50 &&
+                           mouseY < posY + 50 && mouseY > posY- 50) {
+                           
+                           inside = true;
+                           hoveringNode = node;
+                       }
+
+                   })
+               }
+
 
                 for (var j = 0; j < jsons.length; j++) {
                     var json = jsons[j];
@@ -122,13 +155,18 @@
                             json.data[field] = oldIdToNewId[json.data[field]];
 
                     }
+
+                    if(inside) {
+                        json.data["parent"] = hoveringNode.id();
+                    }
+
+
                     if (json.position.x) {
                         if (pasteAtMouseLoc == false) {
                             json.position.x += 50;
                             json.position.y += 50;
 
-                        }
-                        else {
+                        } else {
                             json.position.x += diffX;
                             json.position.y += diffY;
                         }
@@ -147,50 +185,106 @@
                 scratchPad.instance = {
                     copy: function (eles, _id) {
                         var id = _id ? _id : getItemId();
-                        eles.unselect();
+                 
+                       
+
                         var descs = eles.nodes().descendants();
                         var nodes = eles.nodes().union(descs).filter(":visible");
                         var edges = nodes.edgesWith(nodes).filter(":visible");
 
-                        if(options.beforeCopy) {
+                        if (options.beforeCopy) {
                             options.beforeCopy(nodes.union(edges));
                         }
                         clipboard[id] = {nodes: nodes.jsons(), edges: edges.jsons()};
-                        if(options.afterCopy) {
+                        if (options.afterCopy) {
                             options.afterCopy(clipboard[id]);
                         }
+                      
                         return id;
                     },
-                    paste: function (_id, pasteAtMouseLoc) {
+                    paste: function (_id, pasteAtMouseLoc, cuted) {
+                        
                         var id = _id ? _id : getItemId(true);
                         var res = cy.collection();
-                        if(options.beforePaste) {
+                        if (options.beforePaste) {
                             options.beforePaste(clipboard[id]);
                         }
+
+                        if (cuted) {
+
+                        } else {
+
+                        }
+
+
                         if (clipboard[id]) {
-                            var nodes = changeIds(clipboard[id].nodes, pasteAtMouseLoc);
-                            var edges = changeIds(clipboard[id].edges);
+                            //if (cuted == false){
+
+                            var nodes = changeIds(clipboard[id].nodes, pasteAtMouseLoc, cuted);
+                            var edges = changeIds(clipboard[id].edges, pasteAtMouseLoc, cuted);
+
                             oldIdToNewId = {};
                             cy.batch(function () {
                                 res = cy.add(nodes).union(cy.add(edges));
                                 res.select();
                             });
 
+                        
                         }
-                        if(options.afterPaste) {
+                        if (options.afterPaste) {
                             options.afterPaste(res);
                         }
                         cy.trigger('pasteClonedElements');
                         return res;
+                    },
+
+                    cut: function (eles, _id) {
+                   
+
+                        var id = _id ? _id : getItemId();
+                      
+                        var descs = eles.nodes().descendants();
+                        
+                        var nodes = eles.nodes().union(descs).filter(":visible");
+                        var edges = nodes.edgesWith(nodes).filter(":visible");
+
+                        if (options.beforeCut) {
+                            options.beforeCut(nodes.union(edges));
+                        }
+
+                        clipboard[id] = {nodes: nodes.jsons(), edges: edges.jsons()};
+                        eles.remove();
+
+                        if (options.afterCut) {
+                            options.afterCut(clipboard[id]);
+                        }
+
+                        return nodes.union(edges);
+
                     }
                 };
 
                 if (cy.undoRedo) {
                     ur = cy.undoRedo({}, true);
+
                     ur.action("paste", function (eles) {
-                        return eles.firstTime ? scratchPad.instance.paste(eles.id, eles.pasteAtMouseLoc) : eles.restore();
+                        
+
+                        return eles.firstTime ? scratchPad.instance.paste(eles.id, eles.pasteAtMouseLoc, cuted) : eles.restore();
                     }, function (eles) {
+                        
                         return eles.remove();
+                    });
+
+
+                    ur.action("cut", function (eles) {
+
+
+
+                        return eles.firstTime ? scratchPad.instance.cut(cy.$(":selected")) : eles.remove();
+                    }, function (eles) {
+                        
+                        return eles.restore();
                     });
                 }
 
